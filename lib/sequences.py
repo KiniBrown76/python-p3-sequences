d70eddc744386a3caca1584a3ea241841a4df9aa
KiniBrown76 <esther.kini@student.moringaschool.com>
2024-08-31 09:55:01 +0300

    initial commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    elif length < 0:
+        print("Length must be a positive integer.")
+        return
+    
+    fibonacci_sequence = []
+    
+    a, b = 0, 1
+    
+    for _ in range(length):
+        fibonacci_sequence.append(a)
+        a, b = b, a + b
+    
+    print(fibonacci_sequence)
+
+
+print_fibonacci(9)
